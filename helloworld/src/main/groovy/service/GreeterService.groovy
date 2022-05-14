78781cef43c1cc14d05e326832a5f1e6d0abdc5e
Yannick Robin <yannick.robin@sap.com>
2022-05-14 14:41:11 +0800

    a

---
+++ helloworld/src/main/groovy/service/GreeterService.groovy
@@ -0,0 +1,17 @@
+package service;
+
+class GreeterService {
+
+    def configurationService;
+
+    String sayHello(firstName) {
+        def message =  "${getMessage()} ${firstName}";
+        return message;
+    }
+
+    String getMessage()
+    {
+        return configurationService.getConfiguration().getString("easy.helloworld.message");
+    }
+
+}
