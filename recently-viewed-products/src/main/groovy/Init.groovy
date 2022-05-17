0786ffcab9a0dcd2209271d4a4b214383b0eaaed
Amit Panwar <amit.panwar@sap.com>
2022-05-17 21:45:09 +0100

    Update Init.groovy

--- recently-viewed-products/src/main/groovy/Init.groovy
+++ recently-viewed-products/src/main/groovy/Init.groovy
@@ -2,4 +2,5 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
 
 LOG = LoggerFactory.getLogger("recently-viewed-products");
+setupSyncJobService.executeCatalogSyncJob(String.format("%sContentCatalog", "electronics-spa"));
 LOG.info("Init completed...");
