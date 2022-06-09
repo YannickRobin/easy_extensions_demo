77d4c8300a2ead9b743680016c635e0a47ac3ae2
Amit Panwar <amit.panwar@sap.com>
2022-06-09 23:22:53 +0100

    Update Init.groovy

--- helloworld/src/main/groovy/Init.groovy
+++ helloworld/src/main/groovy/Init.groovy
@@ -2,4 +2,8 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader
 
 LOG = LoggerFactory.getLogger("easy_helloworld");
+
+def setupSyncJobService =spring.getBean("setupSyncJobService");
+setupSyncJobService.executeCatalogSyncJob(String.format("%sContentCatalog", "electronics-spa"));
+
 LOG.info("Init completed");
